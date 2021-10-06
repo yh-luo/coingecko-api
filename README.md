44f8f02fa44ea644ea1c4ee5baf2685a103205fc
Yu-Han Luo <yuhanluo1994@gmail.com>
2021-10-07 05:03:21 +0800

    update setup and README

---
+++ README.md
@@ -0,0 +1,19 @@
+# CoinGecko API wrapper (revamped)
+
+Revamped from [pycoingecko](https://github.com/man-c/pycoingecko), a Python3 wrapper around the [CoinGecko API (V3)](https://www.coingecko.com/en/api/documentation).
+
+## Installation
+
+1. Clone or download this folder
+2. Install the package with `pip install .` or put the folder in your project
+
+## Usage
+
+```python
+from coingecko_api import CoinGeckoAPI
+cg = CoinGeckoAPI()
+```
+
+## License
+
+MIT
--- setup.py
+++ setup.py
@@ -2,15 +2,18 @@ from setuptools import setup
 
 from coingecko_api import __version__
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(name='coingecko-api',
       version=__version__,
       packages=['coingecko_api'],
       license='MIT',
-      description='',
-      long_description='',
+      description='Python wrapper around the CoinGecko API',
+      long_description=long_description,
       long_description_content_type="text/markdown",
-      author='',
-      author_email='',
+      author='Yu-Han Luo',
+      author_email='yuhanluo1994@gmail.com',
       install_requires=['requests', 'typing'],
       classifiers=[
           "Programming Language :: Python :: 3",
